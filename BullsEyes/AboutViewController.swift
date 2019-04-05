a2ba8116a7ddca0cc4b8d7a18d7c69f7293e699f
KyungKoo Kang <kgkg0308@sogang.ac.kr>
2019-04-05 04:14:58 -0700

    Adding the Information screen

---
+++ BullsEyes/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  BullsEyes
+//
+//  Created by sgcs on 2019. 4. 5..
+//  Copyright © 2019년 KyungKoo Kang. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated:true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
