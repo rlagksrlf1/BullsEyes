3b3be84848e9ce2072de3a849692cd8791c3ef06
KyungKoo Kang <kgkg0308@sogang.ac.kr>
2019-04-05 03:40:05 -0700

    showing the round on the screen

--- BullsEyes/ViewController.swift
+++ BullsEyes/ViewController.swift
@@ -10,13 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 50
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        startNewRound()
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     
     @IBAction func showAlert(){
-        let message = "The value of the slider is: (currentValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        /*let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"*/
         let alert = UIAlertController(title: "Hello, World", message: message,
                                       preferredStyle: .alert)
         
@@ -24,6 +55,8 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
