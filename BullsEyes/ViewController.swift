2926d68ad64abe4195e0772e6512426f222609d8
KyungKoo Kang <kgkg0308@sogang.ac.kr>
2019-04-05 03:05:07 -0700

    sliders, button and so on UI implication

--- BullsEyes/ViewController.swift
+++ BullsEyes/ViewController.swift
@@ -9,20 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!",
+        let message = "The value of the slider is: (currentValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message,
                                       preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+        print(" The value of the slider is now:\(currentValue)")
     }
 
 }
